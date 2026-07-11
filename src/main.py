e869f9e102d5d66f3ad908cfcab508006c868bfa
Bekir Efe Öztürk <efeoztrk23@proton.me>
2026-07-11 17:49:58 +0300

    feat: add main module

---
+++ src/main.py
@@ -0,0 +1,47 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
+from src.get_bist_companies import get_bist_companies_as_dict_from_redis, get_bist_tickers_as_dict_from_redis, cache_tickers_and_companies
+from src.article_analyzer import generate_deep_report, generate_quick_report
+from src.search_utils import search_companies_by_text
+from src.config import init_config
+import json
+
+app = FastAPI()
+
+# init server
+init_config()
+cache_tickers_and_companies()
+
+@app.get("/")
+def root():
+    return {}
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
+
+@app.get("/bist/companies")
+def bist_companies():
+    return get_bist_companies_as_dict_from_redis()
+
+@app.get("/bist/tickers")
+def bist_tickers():
+    return get_bist_tickers_as_dict_from_redis()
+
+@app.get("/companies/search/{ticker}")
+def search_companies(ticker:str):
+    return search_companies_by_text(ticker)
+
+@app.get("/generate/report/quick/{ticker}")
+def generate_quick_report(ticker: str):
+    return """Hizli Rapor: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."""
+
+@app.get("/generate/report/deep/{ticker}")
+def generate_deep_report(ticker: str):
+    return """Derin Rapor: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."""
+
+@app.get("/montecarlo/{ticker}/{time}")
+def montecarlo(ticker: str):
+    return {f"Sahte tahmin: {ticker} hissesi icin {time} araliginda yukari gitme ihtimali %43"}
+
